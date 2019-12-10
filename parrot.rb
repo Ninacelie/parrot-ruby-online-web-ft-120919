af296b30883bb19d0e8d2acf76a759f5e508d0a8
Nina Angelo <ninacelie@gmail.com>
2019-12-10 01:15:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,3 +2,16 @@ def parrot(phrase = "Squawk!")
   puts "#{phrase}"
   return phrase
 end
+
+parrot("hello there")
+parrot("blah")
+parrot("ophelia")
+parrot("zack")
+parrot("nina")
+
+
+# this thing 
+parrot 
+
+# and this thing are the same
+parrot("Squawk!")
