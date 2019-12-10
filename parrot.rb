54cc986c66aaa40facf5731c30bb0a205c01a4e2
Nina Angelo <ninacelie@gmail.com>
2019-12-10 00:57:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -3,12 +3,3 @@ def parrot(phrase = "Squawk!")
   return phrase
 end
 
-return_value = parrot("this can be anything?")
-puts "return value is #{return_value}"
-
-# def add_two_numbers(number_a, number_b)
-#   number_a + number_b
-# end
-#
-# return_value = add_two_numbers(1000000,23958233)
-# puts "return value of add_two_numbers is #{return_value}"
