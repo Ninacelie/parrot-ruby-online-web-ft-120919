4ff8337d7f10e6439bb4c4669199b7e0558c1194
Nina Angelo <ninacelie@gmail.com>
2019-12-10 00:45:08 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -6,9 +6,9 @@ end
 # return_value = parrot("this can be anything?")
 # puts "return value is #{return_value}"
 
-def add_two_numbers(number_a, number_b)
-  number_a + number_b
-end
-
-return_value = add_two_numbers(1000000,23958233)
-puts "return value of add_two_numbers is #{return_value}"
+# def add_two_numbers(number_a, number_b)
+#   number_a + number_b
+# end
+#
+# return_value = add_two_numbers(1000000,23958233)
+# puts "return value of add_two_numbers is #{return_value}"
